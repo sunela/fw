087cffb5fa1b392aabb4352daf9826ab670ddccb
Werner Almesberger <werner@almesberger.net>
2024-08-16 21:47:52 -0300

    sys/assert.h: hack to replace broken assert(3) of the SDK (WIP)

---
+++ sys/assert.h
@@ -0,0 +1,33 @@
+/*
+ * assert.h - assert(3) replacement
+ *
+ * This work is licensed under the terms of the MIT License.
+ * A copy of the license can be found in the file LICENSE.MIT
+ */
+
+/*
+ * assert(3) of the SDK crashes in fprintf before printing anything. This hack
+ * is more useful.
+ *
+ * @@@ TO DO: don't use this in the simulator, where we have a perfectly good
+ * assert(3).
+ */
+
+#ifndef ASSERT_H
+#define	ASSERT_H
+
+#include <stdlib.h>
+
+#include "debug.h"
+
+
+#define	assert(c)							\
+	do {								\
+		if (!(c)) {						\
+			debug("assertion failed %s:%u: \"%s\"\n",	\
+			    __FILE__, __LINE__, #c);			\
+			while (1);					\
+		}							\
+	} while (0)
+
+#endif /* !ASSERT_H */

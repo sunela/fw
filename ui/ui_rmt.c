061be6f5e5b311d21ce570d4f3c3251ae30ca709
Werner Almesberger <werner@almesberger.net>
2024-08-31 12:09:46 -0300

    ui/ui_rmt.c: oops, forgot to commit the user interface for the "Remote" page
    This also anticipates the "reveal" operation.

---
+++ ui/ui_rmt.c
@@ -0,0 +1,117 @@
+/*
+ * ui_rmt.c - User interface: Remote control
+ *
+ * This work is licensed under the terms of the MIT License.
+ * A copy of the license can be found in the file LICENSE.MIT
+ */
+
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "hal.h"
+#include "db.h"
+#include "gfx.h"
+#include "text.h"
+#include "rmt.h"
+#include "rmt-db.h"
+#include "ui.h"
+#include "ui_rmt.h"
+
+
+#define	FONT	mono24
+
+
+static bool revealing = 0;
+
+
+static void show_remote(void)
+{
+	gfx_clear(&main_da, GFX_BLACK);
+	text_text(&main_da, GFX_WIDTH / 2, GFX_HEIGHT / 2, "Remote",
+	    &FONT, GFX_CENTER, GFX_CENTER, GFX_WHITE);
+
+}
+
+
+/* --- Events -------------------------------------------------------------- */
+
+
+static void ui_rmt_tap(void *ctx, unsigned x, unsigned y)
+{
+	if (revealing) {
+		show_remote();
+		revealing = 0;
+		ui_update_display();
+	} else {
+		ui_return();
+	}
+}
+
+
+static void ui_rmt_tick(void *ctx)
+{
+	rmt_db_poll();
+}
+
+
+static void ui_rmt_to(void *ctx, unsigned from_x, unsigned from_y,
+    unsigned to_x, unsigned to_y, enum ui_swipe swipe)
+{
+	if (swipe == us_left)
+		ui_return();
+}
+
+
+/* --- Open/close ---------------------------------------------------------- */
+
+
+static void ui_rmt_open(void *ctx, void *params)
+{
+	show_remote();
+	rmt_db_init();
+	set_idle(3600); /* @@@ */
+}
+
+
+static void ui_rmt_close(void *ctx)
+{
+	rmt_close(&rmt_usb);
+}
+
+
+/* --- Interface ----------------------------------------------------------- */
+
+
+static const struct ui_events ui_rmt_events = {
+	.touch_tap	= ui_rmt_tap,
+	.touch_to	= ui_rmt_to,
+	.tick		= ui_rmt_tick,
+};
+
+const struct ui ui_rmt = {
+	.name		= "rmt",
+//	.ctx_size	= sizeof(struct ui_rmt_ctx),
+	.open		= ui_rmt_open,
+	.close		= ui_rmt_close,
+	.events		= &ui_rmt_events,
+};
+
+
+/* --- Interface towards the protocol stack -------------------------------- */
+
+
+void ui_rmt_reveal(const struct db_field *f)
+{
+	char buf[f->len + 1];
+
+	memcpy(buf, f->data, f->len);
+	buf[f->len] = 0;
+
+	gfx_clear(&main_da, GFX_BLACK);
+	text_text(&main_da, GFX_WIDTH / 2, GFX_HEIGHT / 2, buf,
+	    &FONT, GFX_CENTER, GFX_CENTER, GFX_YELLOW);
+	revealing = 1;
+	ui_update_display();
+}
---
+++ ui/ui_rmt.h
@@ -0,0 +1,16 @@
+/*
+ * ui_rmt.h - User interface: Remote control
+ *
+ * This work is licensed under the terms of the MIT License.
+ * A copy of the license can be found in the file LICENSE.MIT
+ */
+
+#ifndef UI_RMT_H
+#define	UI_RMT_H
+
+#include "db.h"
+
+
+void ui_rmt_reveal(const struct db_field *f);
+
+#endif /* !UI_RMT_H */

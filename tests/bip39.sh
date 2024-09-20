20caaf0dd85cb45f19f565b5edb20ef7cbf09fcf
Werner Almesberger <werner@almesberger.net>
2024-09-20 13:33:43 -0300

    tests/bip39.sh: test the BIP-0039 test vectors (24 vectors)

---
+++ tests/bip39.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# bip39.sh - Test the BIP39 reference vectors
+#
+# This work is licensed under the terms of the MIT License.
+# A copy of the license can be found in the file LICENSE.MIT
+#
+
+VECTORS=../lib/bip39/vectors.json
+
+jq -r '.english[] | .[0]+" "+.[1]' $VECTORS | while read l; do
+	set - $l
+	hex=$1
+	shift
+	res=`../sim -q -C "bip39 $hex"` || exit
+	if [ "$*" != "$res" ]; then
+		cat <<EOF 2>&2
+Vector mismatch:
+Input:    $hex
+Output:   $res
+Expected: $*
+EOF
+		exit 1
+	fi
+done

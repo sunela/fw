21f638d90da258387fb7818d54c867af05bb2aac
Werner Almesberger <werner@almesberger.net>
2024-09-25 22:50:14 -0300

    tests/bip39.sh: also test decoding

--- tests/bip39.sh
+++ tests/bip39.sh
@@ -8,10 +8,31 @@
 
 VECTORS=../lib/bip39/vectors.json
 
+
+usage()
+{
+	echo "usage: $0 [-v] [-x]" 1>&2
+	exit 1
+}
+
+
+verbose=false
+while [ "$1" ]; do
+	case "$1" in
+	-v)	verbose=true
+		shift;;
+	-x)	set -x
+		shift;;
+	*)	usage;;
+	esac
+done
+
 jq -r '.english[] | .[0]+" "+.[1]' $VECTORS | while read l; do
 	set - $l
 	hex=$1
 	shift
+
+	$verbose && echo "$hex -> $res" 1>&2
 	res=`../sim -q -C "bip39 encode $hex"` || exit
 	if [ "$*" != "$res" ]; then
 		cat <<EOF 2>&2
@@ -19,6 +40,18 @@ Vector mismatch:
 Input:    $hex
 Output:   $res
 Expected: $*
+EOF
+		exit 1
+	fi
+
+	$verbose && echo "$* -> $hex" 1>&2
+	res=`../sim -q -C "bip39 decode $*"` || exit
+	if [ "$hex" != "$res" ]; then
+		cat <<EOF 2>&2
+Vector mismatch:
+Input:    $*
+Output:   $res
+Expected: $hex
 EOF
 		exit 1
 	fi
